63b142259f551fd5d76639b6c6d86ac4d545d7d2
arav <aravadhikari05@gmail.com>
2026-04-08 01:11:56 -0700

    fix: more vercel analytics bugs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { inject } from '@vercel/analytics'
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App'
 
-inject()
 
 
 createRoot(document.getElementById('root')!).render(
